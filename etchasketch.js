9ac12bcadf47af39a6e586f07e7531c1174f39bc
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2024-01-16 12:29:10 -0800

    Create button that will allow the user to choose the grid size.

--- etchasketch.js
+++ etchasketch.js
@@ -1,19 +1,38 @@
 let container = document.querySelector('#container');
-for(let i = 0; i < 16; i++){
-    let new_div = document.createElement('div');
-    new_div.className = 'row';
-    for(let j = 0; j < 16; j++){
-        let new_new_div = document.createElement('div');
-        new_new_div.className = 'grid column';
-        new_div.appendChild(new_new_div);
+
+function deleteGrid(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
     }
-    container.appendChild(new_div);
 }
 
-let grid = document.querySelectorAll('.grid');
-
-for(let i = 0; i < grid.length; i++){
-    grid[i].addEventListener('mouseover', (e) =>{
-        grid[i].style.backgroundColor = 'black';
-    });
+function createGrid(gridSize){
+    deleteGrid();
+    for(let i = 0; i < gridSize; i++){
+        let new_div = document.createElement('div');
+        new_div.className = 'row';
+        for(let j = 0; j < size; j++){
+            let new_new_div = document.createElement('div');
+            new_new_div.className = 'grid column';
+            new_div.appendChild(new_new_div);
+        }
+        container.appendChild(new_div);
+    }
+    //Allow drawing via hovering
+    let grid = document.querySelectorAll('.grid');
+    for(let i = 0; i < grid.length; i++){
+        grid[i].addEventListener('mouseover', (e) =>{
+            grid[i].style.backgroundColor = 'black';
+        });
+    }
 }
+
+let sizeSelector = document.querySelector('#sizeSelector');
+
+sizeSelector.addEventListener('click', (e) => {
+    let size = prompt("What size do you want it to be? 1-100.");
+    createGrid(size);
+});
+
+//Create initial grid
+createGrid(16);
